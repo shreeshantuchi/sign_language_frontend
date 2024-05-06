8a08dae6d4d077f3317d34ad7daad6ee59ede425
shreeshantuchi <82638369+shreeshantuchi@users.noreply.github.com>
2024-05-06 14:00:38 +0545

    added dictionary search to create text

--- lib/screen/reverseSignScreen/reverse_screen.dart
+++ lib/screen/reverseSignScreen/reverse_screen.dart
@@ -1,9 +1,14 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
+import 'package:searchfield/searchfield.dart';
 import 'package:sign_language_record_app/Api/dictionary_api.dart';
 import 'package:sign_language_record_app/provider/reverseScreenProvider/reverese_screen_provider.dart';
 import 'package:sign_language_record_app/provider/signDetectState/sign_detect_state_Provider.dart';
+import 'package:sign_language_record_app/screen/videoEditorScreen/video_editor_screen.dart';
 import 'package:sign_language_record_app/screen/videoPlayerScreen/video_player.dart';
 import 'package:sign_language_record_app/widget/app_button.dart';
 import 'package:provider/provider.dart';
@@ -16,11 +21,19 @@ class ReverseScreen extends StatefulWidget {
 }
 
 class _ReverseScreenState extends State<ReverseScreen> {
-  TextEditingController textEditingController = TextEditingController();
+  Future? _future;
+
   @override
   void dispose() {
     // TODO: implement dispose
     super.dispose();
+  }
+
+  @override
+  void initState() {
+    _future = context.read<DictionaryAPi>().getDectionary();
+    // TODO: implement initState
+    super.initState();
   }
 
   @override
@@ -82,27 +95,177 @@ class _ReverseScreenState extends State<ReverseScreen> {
   }
 
   Row searchField() {
+    List<String> listWords = [];
     return Row(
       children: [
         SizedBox(
-          width: 250,
-          child: TextField(
-            onTap: () => context.read<SignProvider>().updateFocus(true),
-            onSubmitted: (value) {
-              context.read<SignProvider>().updateFocus(false);
-              context
-                  .read<RevereseScreenProvider>()
-                  .getReverseSignVideo(textEditingController.text);
+          width: 400,
+          child: FutureBuilder(
+              future: _future,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return SearchFieldSample(
+                    textEditingController: textEditingController,
+                  );
+                } else {
+                  return const SizedBox.shrink();
+                }
+              }),
+          //  TextField(
+          //   onTap: () => context.read<SignProvider>().updateFocus(true),
+          //   onSubmitted: (value) {
+          //     context.read<SignProvider>().updateFocus(false);
+          //     context
+          //         .read<RevereseScreenProvider>()
+          //         .getReverseSignVideo(textEditingController.text);
+          //   },
+          //   decoration: const InputDecoration(
+          //       hintText: "Enter Your Text",
+          //       border: OutlineInputBorder(
+          //           borderSide: BorderSide(
+          //               width: 3.0,
+          //               color: Colors.black // Adjust the width of the border
+          //               ),
+          //           borderRadius: BorderRadius.all(const Radius.circular(20)))),
+          //   controller: textEditingController,
+          // ),
+        ),
+      ],
+    );
+  }
+}
+
+class SearchFieldSample extends StatefulWidget {
+  const SearchFieldSample({
+    super.key,
+    required this.textEditingController,
+  });
+  final TextEditingController textEditingController;
+
+  @override
+  State<SearchFieldSample> createState() => _SearchFieldSampleState();
+}
+
+class _SearchFieldSampleState extends State<SearchFieldSample> {
+  List<String> individualString = [];
+  int suggestionsCount = 12;
+  final focus = FocusNode();
+
+  @override
+  void initState() {
+    suggestions =
+        List.generate(suggestionsCount, (index) => 'suggestion $index');
+    super.initState();
+  }
+
+  String joinWords(List<String> words) {
+    return words.join(' ');
+  }
+
+  List<String> stringToListOfWords(String sentence) {
+    // Split the sentence into words based on whitespace
+    List<String> words = sentence.split(' ');
+    return words;
+  }
+
+  String getLastWord(String sentence) {
+    // Split the sentence into words
+    List<String> words = sentence.split(' ');
+
+    // Get the last word
+    String lastWord = words.isNotEmpty ? words.last : '';
+
+    return lastWord;
+  }
+
+  var suggestions = <String>[];
+  @override
+  Widget build(BuildContext context) {
+    Widget searchChild(x) => Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4.0, horizontal: 12),
+          child: Text(x,
+              style: const TextStyle(fontSize: 24, color: Colors.black)),
+        );
+    return Row(
+      children: [
+        Container(
+          width: 200,
+          child: SearchField(
+            suggestionDirection: SuggestionDirection.up,
+            onSearchTextChanged: (query) {
+              individualString = stringToListOfWords(query);
+
+              context.read<DictionaryAPi>().filterSearch(getLastWord(query));
+              return context
+                  .read<DictionaryAPi>()
+                  .filteredDictionary
+                  .map((e) => SearchFieldListItem<String>(e.name,
+                      child: searchChild(e.name)))
+                  .toList();
             },
-            decoration: const InputDecoration(
-                hintText: "Enter Your Text",
-                border: OutlineInputBorder(
-                    borderSide: BorderSide(
-                        width: 3.0,
-                        color: Colors.black // Adjust the width of the border
-                        ),
-                    borderRadius: BorderRadius.all(const Radius.circular(20)))),
+            onTap: () {},
+            autovalidateMode: AutovalidateMode.onUserInteraction,
+            validator: (value) {
+              if (value == null || value.length < 4) {
+                return 'error';
+              }
+              return null;
+            },
+            key: const Key('searchfield'),
+            hint: 'Enter your Text',
             controller: textEditingController,
+            itemHeight: 50,
+            scrollbarDecoration: ScrollbarDecoration(),
+            //   thumbVisibility: true,
+            //   thumbColor: Colors.red,
+            //   fadeDuration: const Duration(milliseconds: 3000),
+            //   trackColor: Colors.blue,
+            //   trackRadius: const Radius.circular(10),
+            // ),
+            onTapOutside: (x) {},
+            suggestionStyle: const TextStyle(fontSize: 24, color: Colors.black),
+            searchInputDecoration: InputDecoration(
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(24),
+                borderSide: const BorderSide(
+                  width: 1,
+                  color: Colors.orange,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(24),
+                borderSide: const BorderSide(
+                  width: 1,
+                  color: Colors.black,
+                  style: BorderStyle.solid,
+                ),
+              ),
+              fillColor: Colors.white,
+              filled: true,
+              contentPadding: const EdgeInsets.symmetric(
+                horizontal: 20,
+              ),
+            ),
+            suggestionsDecoration: SuggestionDecoration(
+              color: Colors.white,
+              border: Border.all(color: Colors.orange),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            suggestions: context
+                .read<DictionaryAPi>()
+                .filteredDictionary
+                .map((e) => SearchFieldListItem<String>(e.name,
+                    child: searchChild(e.name)))
+                .toList(),
+            focusNode: focus,
+            suggestionState: Suggestion.expand,
+            onSuggestionTap: (SearchFieldListItem<String> x) {
+              print(textEditingController.text);
+              individualString.removeLast();
+              individualString.add(x.searchKey);
+              textEditingController.text = joinWords(individualString);
+            },
           ),
         ),
         const SizedBox(
@@ -149,9 +312,9 @@ class SwitchWidget extends StatelessWidget {
               ),
             );
           case ReverseScreenState.done:
-            return VideoStack();
+            return const VideoStack();
           default:
-            return SizedBox.shrink();
+            return const SizedBox.shrink();
         }
       },
     );
@@ -180,7 +343,7 @@ class SwitchVideoWidget extends StatelessWidget {
                 scalVideo: true,
                 videoUrl: "http://10.0.2.2:8000/media/output/normal_video.mp4");
           default:
-            return SizedBox.shrink();
+            return const SizedBox.shrink();
         }
       },
     );
@@ -201,7 +364,7 @@ class _VideoStackState extends State<VideoStack> {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        SwitchVideoWidget(),
+        const SwitchVideoWidget(),
         IconButton(
           onPressed: () =>
               context.read<RevereseScreenProvider>().change == ChangeState.first
